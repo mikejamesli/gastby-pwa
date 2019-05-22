4b7cb5bb6771508030ba2c40a43eae312a7398dd
Michael James <mikejlyh@gmail.com>
2019-05-22 22:01:19 +0100

    mive pwa manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,13 +13,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Mive Blog`,
+        short_name: `MiveBlog`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#524763`,
+        theme_color: `#524763`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     {
